21001d73ec721751a3db6d7bbf3058a6c1fd21eb
NILESH KALE <111424960+Nilesh-2119@users.noreply.github.com>
2025-12-03 21:11:44 +0530

    Update urls.py

--- pathology_backend/expenses/urls.py
+++ pathology_backend/expenses/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ExpenseListCreateView, ExpenseDetailView
 
 urlpatterns = [
-    path('', ExpenseListCreateView.as_view(), name='expense-list-create'),
-    path('<int:pk>/', ExpenseDetailView.as_view(), name='expense-detail'),
+    path("", ExpenseListCreateView.as_view(), name="expense-list"),
+    path("<int:pk>/", ExpenseDetailView.as_view(), name="expense-detail"),
 ]
